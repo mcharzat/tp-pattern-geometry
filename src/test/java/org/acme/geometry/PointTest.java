3e8882dd6a70440c033dafb4685fb625aca61bd1
Maxime_Charzat <23charzatcav@gmail.com>
2022-01-03 14:49:13 +0100

    Implementation of test for translate method for Point class

--- src/test/java/org/acme/geometry/PointTest.java
+++ src/test/java/org/acme/geometry/PointTest.java
@@ -22,4 +22,14 @@ public class PointTest {
         Assert.assertEquals(c, p.getCoordinate());
 		Assert.assertFalse(p.isEmpty());
     }
+
+    @Test
+    public void testTranslate() {
+        Coordinate c = new Coordinate(3.14, 2.72);
+        Point p = new Point(c);
+        p.translate(10, 10);
+        Assert.assertEquals(13.14, p.getCoordinate().getX(), EPSILON);
+        Assert.assertEquals(12.72, p.getCoordinate().getY(), EPSILON);
+		Assert.assertFalse(p.isEmpty());
+    }
 }
