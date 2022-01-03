219abb92294ab42ae59e9e888c09cb930b22fc5e
Maxime_Charzat <23charzatcav@gmail.com>
2022-01-03 14:49:40 +0100

    Implementation of test for translate method for LineString class

--- src/test/java/org/acme/geometry/LineStringTest.java
+++ src/test/java/org/acme/geometry/LineStringTest.java
@@ -29,4 +29,23 @@ public class LineStringTest {
         Assert.assertEquals(p, l.getPointN(0));
 		Assert.assertFalse(l.isEmpty());
     }
+
+    @Test
+    public void testTranslate() {
+        List<Point> points = new ArrayList<Point>();
+        Coordinate c1 = new Coordinate(3.14, 2.72);
+        Point p1 = new Point(c1);
+        points.add(p1);
+        Coordinate c2 = new Coordinate(31.4, 27.2);
+        Point p2 = new Point(c2);
+        points.add(p2);
+
+        LineString l = new LineString(points);
+        l.translate(10, 10);
+        Assert.assertEquals(13.14, l.getPointN(0).getCoordinate().getX(), EPSILON);
+        Assert.assertEquals(12.72, l.getPointN(0).getCoordinate().getY(), EPSILON);
+        Assert.assertEquals(41.4, l.getPointN(1).getCoordinate().getX(), EPSILON);
+        Assert.assertEquals(37.2, l.getPointN(1).getCoordinate().getY(), EPSILON);
+		Assert.assertFalse(l.isEmpty());
+    }
 }
