1038ab261069e2dcfbaf75535c5e7d215bac767b
Maxime_Charzat <23charzatcav@gmail.com>
2022-01-03 16:51:28 +0100

    Add envelopeBuilder and test

---
+++ src/main/java/org/acme/geometry/EnvelopeBuilder.java
@@ -0,0 +1,51 @@
+package org.acme.geometry;
+
+public class EnvelopeBuilder {
+
+    protected Envelope envelope;
+
+    public EnvelopeBuilder() {
+        this.envelope = new Envelope();
+    }
+
+    public void insert(Coordinate coordinate){
+        if (this.envelope.isEmpty()) this.envelope = new Envelope(coordinate, coordinate);
+        else {
+            if (this.envelope.getXmin() > coordinate.getX()){
+                Coordinate topRight = new Coordinate(this.envelope.getXmax(),this.envelope.getYmax());
+                if (this.envelope.getYmin() > coordinate.getY())
+                this.envelope = new Envelope(coordinate, topRight);
+                else {
+                    Coordinate bottomLeft = new Coordinate(coordinate.getX(),this.envelope.getYmin());
+                    this.envelope = new Envelope(bottomLeft, topRight);
+                }
+            }
+
+            else if (this.envelope.getYmin() > coordinate.getY()){
+                Coordinate topRight = new Coordinate(this.envelope.getXmax(),this.envelope.getYmax());
+                Coordinate bottomLeft = new Coordinate(this.envelope.getXmin(), coordinate.getY());
+                this.envelope = new Envelope(bottomLeft, topRight);
+            }
+
+            if (this.envelope.getXmax() < coordinate.getX()) {
+                Coordinate bottomLeft = new Coordinate(this.envelope.getXmin(),this.envelope.getYmin());
+                if (this.envelope.getYmax() < coordinate.getY())
+                this.envelope = new Envelope(bottomLeft, coordinate);
+                else {
+                    Coordinate topRight = new Coordinate(coordinate.getX(),this.envelope.getYmax());
+                    this.envelope = new Envelope(bottomLeft, topRight);
+                }
+            }
+
+            else if (this.envelope.getYmax() < coordinate.getY()){
+                Coordinate bottomLeft = new Coordinate(this.envelope.getXmin(),this.envelope.getYmin());
+                Coordinate topRight = new Coordinate(this.envelope.getXmax(), coordinate.getY());
+                this.envelope = new Envelope(bottomLeft, topRight);
+            }
+        }
+    }
+
+    public Envelope build(){
+        return this.envelope;
+    }
+}
