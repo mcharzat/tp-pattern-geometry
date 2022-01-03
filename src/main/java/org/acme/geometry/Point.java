e8a3765616c38de661cb3254358276b9e9fca3b9
Maxime_Charzat <23charzatcav@gmail.com>
2022-01-03 14:16:03 +0100

    Adapt and verify default initialisation

--- src/main/java/org/acme/geometry/Point.java
+++ src/main/java/org/acme/geometry/Point.java
@@ -20,4 +20,9 @@ public class Point implements Geometry{
     public String getType() {
         return "Point";
     }
+
+    @Override
+    public boolean isEmpty() {
+        return Double.isNaN(coordinate.getX());
+    }
 }
