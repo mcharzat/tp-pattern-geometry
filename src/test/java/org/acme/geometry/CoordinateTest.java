1553e7aa298161a9796576eb191f8b6bc97e201d
Maxime_Charzat <23charzatcav@gmail.com>
2022-01-03 14:30:42 +0100

    Add test for constructor

--- src/test/java/org/acme/geometry/CoordinateTest.java
+++ src/test/java/org/acme/geometry/CoordinateTest.java
@@ -9,10 +9,17 @@ public class CoordinateTest {
 
 	@Test
 	public void testDefaultConstructor(){
-		// TODO
 		Coordinate c = new Coordinate();
 		Assert.assertEquals(Double.NaN, c.getX(), EPSILON);
 		Assert.assertEquals(Double.NaN, c.getY(), EPSILON);
+		Assert.assertTrue(c.isEmpty());
 	}
 
+	@Test
+	public void testConstructor(){
+		Coordinate c = new Coordinate(3.14, 2.72);
+		Assert.assertEquals(3.14, c.getX(), EPSILON);
+		Assert.assertEquals(2.72, c.getY(), EPSILON);
+		Assert.assertFalse(c.isEmpty());
+	}
 }
