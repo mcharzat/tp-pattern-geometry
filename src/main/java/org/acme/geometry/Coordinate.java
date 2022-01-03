3489906ec61f2f808b3c20abf029eb9a00c4b5e5
Maxime_Charzat <23charzatcav@gmail.com>
2022-01-03 13:44:54 +0100

    Implementation of Coordinate class

--- src/main/java/org/acme/geometry/Coordinate.java
+++ src/main/java/org/acme/geometry/Coordinate.java
@@ -2,4 +2,24 @@ package org.acme.geometry;
 
 public class Coordinate {
 
+    private final double x;
+    private final double y;
+
+    public Coordinate(){
+        this.x = 0.0;
+        this.y = 0.0;
+    }
+
+    public Coordinate(double x, double y){
+        this.x = x;
+        this.y = y;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
 }
