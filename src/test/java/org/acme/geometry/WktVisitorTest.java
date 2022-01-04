edaced04d281332e0c867bbdd4975927f9254f11
Maxime_Charzat <23charzatcav@gmail.com>
2022-01-04 11:28:06 +0100

    Implementation of tests for WktVisitor class

---
+++ src/test/java/org/acme/geometry/WktVisitorTest.java
@@ -0,0 +1,47 @@
+package org.acme.geometry;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class WktVisitorTest {
+
+    @Test
+    public void testVisitEmptyPoint(){
+        Point p = new Point();
+
+        WktVisitor visitor = new WktVisitor();
+        p.accept(visitor);
+
+        Assert.assertEquals("POINT EMPTY", visitor.getResult());
+    }
+
+    @Test
+    public void testVisitPoint(){
+        Point p = SampleFactory.createPointA();
+
+        WktVisitor visitor = new WktVisitor();
+        p.accept(visitor);
+
+        Assert.assertEquals("POINT(3.14 2.72)", visitor.getResult());
+    }
+
+    @Test
+    public void testVisitEmptyLineString(){
+        LineString l = new LineString();
+
+        WktVisitor visitor = new WktVisitor();
+        l.accept(visitor);
+
+        Assert.assertEquals("LINESTRING EMPTY", visitor.getResult());
+    }
+
+    @Test
+    public void testVisitLineString(){
+        LineString l = SampleFactory.createLineStringAB();
+
+        WktVisitor visitor = new WktVisitor();
+        l.accept(visitor);
+
+        Assert.assertEquals("LINESTRING(3.14 2.72,31.4 27.2)", visitor.getResult());
+    }
+}
