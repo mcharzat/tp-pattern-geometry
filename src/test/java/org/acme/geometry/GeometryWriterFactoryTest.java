6d67651ce498a82949e20fe5409e984d6ffccac9
Maxime_Charzat <23charzatcav@gmail.com>
2022-01-04 16:36:34 +0100

    Change test of GeometryWriterFactory

--- src/test/java/org/acme/geometry/GeometryWriterFactoryTest.java
+++ src/test/java/org/acme/geometry/GeometryWriterFactoryTest.java
@@ -13,5 +13,9 @@ public class GeometryWriterFactoryTest {
         String formatName = "WKT";
         GeometryWriter writer = writerFactory.createGeometryWriter(formatName);
         Assert.assertEquals("POINT(3.14 2.72)", writer.write(point));
+
+        String formatName2 = "GeoJSON";
+        GeometryWriter writer2 = writerFactory.createGeometryWriter(formatName2);
+        Assert.assertEquals("{'type':'Point'}", writer2.write(point));
     }
 }
